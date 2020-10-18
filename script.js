02d5df13e5073ee5be773fb2a27f1edd41353f13
Kot1que <me@krvd.ru>
2020-10-18 20:42:49 +0300

    Main city done

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 document.body.onload = function() {
+    let geo = navigator.geolocation;
+    let latitude;
+    let longitude;
 
+    geo.getCurrentPosition(position => {
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
+    }, positionError => {
+        // ITMO Kronverksky 49
+        latitude = 59.9571;
+        longitude = 30.3084;
+    })
+
+
+    setTimeout(function () {
+        document.getElementById("main-city-wrapper").style.display = "flex";
+        document.getElementById("main-city-loader").style.display = "none";
+    }, 200)
 }
